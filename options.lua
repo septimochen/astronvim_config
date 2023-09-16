e1daf6f56fa61f7b6cd4232bd65543ef2fbd8a64
septimochen <qizu.chen@hotmail.com>
2023-09-16 20:33:10 +0800

    update

---
+++ options.lua
@@ -0,0 +1,16 @@
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    vim.bo.tabstop = 4
+    vim.bo.softtabstop = 4
+    vim.bo.shiftwidth = 4
+    vim.bo.expandtab = true
+  end,
+  desc = "Set tabs to 4 chars in Go files",
+})
