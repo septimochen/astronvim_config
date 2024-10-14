a6d9737332ca96eab5f5f6d3c39bc0a60521d208
septimochen <qizu.chen@hotmail.com>
2024-10-14 13:27:38 +0800

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,7 +13,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         "pylsp",
-        "ruff_lsp",
+        "ruff",
         "gopls",
         "rust_analyzer",
         -- add more arguments for adding more language servers
